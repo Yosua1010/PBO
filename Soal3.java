597bd29b7b5fcb73ed2a44312fb7e2f1973a8d65
Yosua1010 <134156321+Yosua1010@users.noreply.github.com>
2023-09-03 08:07:23 +0700

    Add files via upload

---
+++ Soal3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Soal3 {
+     public static void main(String[] args) {
+     double x = 92.98;
+     int nx = (int) Math.round(x);
+         System.out.println(""+nx);
+     }
+}
