dd13500ae50b232b3e72c50960e8f58218397fae
Yosua1010 <134156321+Yosua1010@users.noreply.github.com>
2023-09-08 18:42:24 +0700

    Add files via upload

--- Soal1.java
+++ Soal1.java
@@ -1,50 +1,23 @@
- /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-package com.mycompany.soal1;
-
 import java.util.Scanner;
-/**
- *
- * @author LENOVO
- */
+
 public class Soal1 {
-
     public static void main(String[] args) {
-       
         Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt(); // Membaca jumlah kasus uji
+        String s = scanner.nextLine();
 
-        for (int i = 0; i < T; i++) {
-            try {
-                long n = scanner.nextLong(); // Membaca input integer
+        // Memecah string input menjadi token menggunakan ekspresi reguler
+        String[] tokens = s.split("[^A-Za-z]+");
 
-                System.out.println(n + " can be fitted in:");
+        // Menampilkan jumlah token
+        System.out.println(tokens.length);
 
-                if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE) {
-                    System.out.println("* byte");
-                }
-                if (n >= Short.MIN_VALUE && n <= Short.MAX_VALUE) {
-                    System.out.println("* short");
-                }
-                if (n >= Integer.MIN_VALUE && n <= Integer.MAX_VALUE) {
-                    System.out.println("* int");
-                }
-                // Tidak perlu mengecek range untuk long, karena n sudah bertipe long
-                System.out.println("* long");
-            } catch (Exception e) {
-                System.out.println(scanner.next() + " can't be fitted anywhere.");
+        // Menampilkan setiap token di baris baru
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                System.out.println(token);
             }
         }
 
         scanner.close();
-    
-    
-
-
     }
-    
 }
-
