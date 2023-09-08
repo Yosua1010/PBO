98ba4da3020b4dc8345cef5ac8d7e2e6eba6dd6a
Yosua1010 <134156321+Yosua1010@users.noreply.github.com>
2023-09-08 21:24:37 +0700

    Add files via upload
    Soal1 minggu 3/w3

---
+++ Soal1_InpuOutput_W3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Soal1_InpuOutput_W3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+
+        // Memecah string input menjadi token menggunakan ekspresi reguler
+        String[] tokens = s.split("[^A-Za-z]+");
+
+        // Menampilkan jumlah token
+        System.out.println(tokens.length);
+
+        // Menampilkan setiap token di baris baru
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                System.out.println(token);
+            }
+        }
+
+        scanner.close();
+    }
+}
