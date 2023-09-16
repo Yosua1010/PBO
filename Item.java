d6f97dae096603b2a42ae0f9ac2aaba86519f2a7
Yosua1010 <134156321+Yosua1010@users.noreply.github.com>
2023-09-16 18:31:18 +0700

    Add files via upload
    pertemuan ke4: Kelas Item memiliki sebuah konstruktor yang menerima nama sebagai parameter dan mencetak nama tersebut ke konso

---
+++ Item.java
@@ -0,0 +1,11 @@
+// Item.java
+package com.mycompany.upinipin;
+
+public class Item {
+    private String itemName;
+
+    public Item(String name) {
+        this.itemName = name;
+        System.out.println(this.itemName);
+    }
+}
