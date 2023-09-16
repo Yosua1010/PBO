9da47912bba4ecc403d5a8b0f3d6f5d45ca9a4b1
Yosua1010 <134156321+Yosua1010@users.noreply.github.com>
2023-09-16 18:26:58 +0700

    Add files via upload
    Pertemuan ke4: Kelas UpinIpin memiliki metode main sebagai titik masuk program

---
+++ UpinIpin.java
@@ -0,0 +1,9 @@
+// UpinIpin.java
+package com.mycompany.upinipin;
+
+public class UpinIpin {
+    public static void main(String[] args) {
+        // Membuat objek Item dengan nama "upin"
+        Item name = new Item("Ipin");
+    }
+}
