1fe87aa4b6b19661336e9c121991c261c0417d3c
Yosua1010 <134156321+Yosua1010@users.noreply.github.com>
2023-09-16 18:44:28 +0700

    Add files via upload
    pertemuan k4: Kelas Item memiliki sebuah konstruktor yang menerima nama sebagai parameter dan mencetak nama tersebut ke konsol

--- Item.java
+++ Item.java
@@ -9,3 +9,18 @@ public class Item {
         System.out.println(this.itemName);
     }
 }
+
+/*
+ini code dari class UpinIpin, saya tulis sini karena
+saat saya upload di git tidak tampil
+// UpinIpin.java
+package com.mycompany.upinipin;
+
+public class UpinIpin {
+    public static void main(String[] args) {
+        // Membuat objek Item dengan nama "upin"
+        Item name = new Item("Ipin");
+    }
+}
+
+*/
